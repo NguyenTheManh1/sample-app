cba6a099704ec415e47e8a19e0e06b58c6a27d48
Nguyen The Manh <manhnt0210@gmail.com>
2019-07-25 20:45:04 +0700

    Chapter 11: Account activation

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 2019_07_24_074111) do
+ActiveRecord::Schema.define(version: 2019_07_25_021001) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -8,6 +8,9 @@ ActiveRecord::Schema.define(version: 2019_07_24_074111) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
